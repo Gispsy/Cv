d5b900d0b41130ea458c50b7ff32d9ee2f3c2ceb
Jarod Ducamp <ducamp.jarod3011@gmail.com>
2023-06-28 15:38:47 +0200

    Modification cv fini Post possible

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ function Voir() {
     document.getElementById('profil-btn-voir').style.display = 'none';
 }
 
-function Fermer(){
+function Fermer() {
     document.getElementById('github').style.display = 'none';
     document.getElementById('img-git').style.display = 'none';
     document.getElementById('codepen').style.display = 'none';
@@ -39,4 +39,39 @@ function Retour(detailsId) {
     document.getElementById(detailsId).style.display = 'none';
     document.getElementById(projets.find(p => p.detailsId === detailsId).projetId).style.display = 'block';
     console.log("FIn Retour");
+}
+
+function reloadElements() {
+    // Sélectionner les barres de progression des frameworks
+    var bootstrapBarre = document.querySelector('.fa-bootstrap + .framework-icone + .framework-base .framework-barre');
+    var symfonyBarre = document.querySelector('.fa-symfony + .framework-icone + .framework-base .framework-barre');
+    var reactBarre = document.querySelector('.fa-react + .framework-icone + .framework-base .framework-barre');
+
+    // Réinitialiser la largeur des barres de progression à 0%
+    bootstrapBarre.style.width = '0%';
+    symfonyBarre.style.width = '0%';
+    reactBarre.style.width = '0%';
+
+    // Générer des valeurs aléatoires pour la progression
+    var bootstrapProgress = Math.random() * 100;
+    var symfonyProgress = Math.random() * 100;
+    var reactProgress = Math.random() * 100;
+
+    // Mettre à jour la largeur des barres de progression avec les valeurs aléatoires
+    bootstrapBarre.style.width = bootstrapProgress + '%';
+    symfonyBarre.style.width = symfonyProgress + '%';
+    reactBarre.style.width = reactProgress + '%';
+
+    // Ajouter la classe 'reset' pour réinitialiser l'animation
+    bootstrapBarre.classList.add('reset');
+    symfonyBarre.classList.add('reset');
+    reactBarre.classList.add('reset');
+
+    // Attendre un court instant avant de supprimer la classe 'reset' pour activer l'animation
+    setTimeout(function () {
+        // Supprimer la classe 'reset' pour activer l'animation
+        bootstrapBarre.classList.remove('reset');
+        symfonyBarre.classList.remove('reset');
+        reactBarre.classList.remove('reset');
+    }, 100);
 }
